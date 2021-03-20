72b36347700ddad5de85d5446703b8798239b3b4
Tiaguin061 <tiaguin180@gmail.com>
2021-03-20 11:05:15 -0300

    feat: Adding method create in Interface

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,6 +1,8 @@
+import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
 import Appointment from '../infra/typeorm/entities/Appointment';
 
+
 export default interface IAppointmentsRepository {
-  // create(): Promise<Appointment>;
+  create(data: ICreateAppointmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
 }
