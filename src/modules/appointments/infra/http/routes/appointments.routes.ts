bc59e3fb4164f20e6f9450dd648253401fa2efe4
Tiaguin061 <tiaguin180@gmail.com>
2021-03-21 12:39:11 -0300

    refactor: Putting dependency injection on the routes

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
 
-import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
+import {container} from 'tsyringe';
+
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
@@ -21,8 +22,7 @@ appointmentsRouter.post('/', async (request, response) => {
   // parseIso só esta transformando o dado
   const parsedDate = parseISO(date);
 
-  const appointmentsRepository = new AppointmentsRepository()
-  const createAppointmentService = new CreateAppointmentService(appointmentsRepository);
+  const createAppointmentService = container.resolve(CreateAppointmentService);
 
   const appointment = await createAppointmentService.execute({
     date: parsedDate, 
