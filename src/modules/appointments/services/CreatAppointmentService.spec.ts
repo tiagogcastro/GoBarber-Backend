dff195bc1602ed8411bff4bb859ee407a291cb77
Tiaguin061 <tiaguin180@gmail.com>
2021-03-25 11:32:32 -0300

    feat: Adding appointment scheduling test

--- src/modules/appointments/services/CreatAppointmentService.spec.ts
+++ src/modules/appointments/services/CreatAppointmentService.spec.ts
@@ -1,3 +1,40 @@
-test('Sum two numbers' , () => {
-  expect(1 + 2).toBe(3);
+import AppError from '@shared/errors/AppError';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppontimentRepository';
+
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('Should be able to create a new appointment', async () => {
+    const fakeAppontimentRepository = new FakeAppointmentsRepository();
+
+    const createAppointment = new CreateAppointmentService(fakeAppontimentRepository);
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123414',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123414');
+  });
+
+  it('Should not be able to create two appointment on the same time', async () => {
+    const fakeAppontimentRepository = new FakeAppointmentsRepository();
+
+    const createAppointment = new CreateAppointmentService(fakeAppontimentRepository);
+
+    const appointmentDate = new Date(2020, 4, 10, 11)
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '123123414',
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '123123414',
+      })
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
