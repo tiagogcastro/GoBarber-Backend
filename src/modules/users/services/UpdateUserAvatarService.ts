59440eca65d8269c64bfa92b852ca34a72e58683
Tiaguin061 <tiaguin180@gmail.com>
2021-03-29 16:00:02 -0300

    refactor: Using storage provider

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -7,6 +7,7 @@ import User from '../infra/typeorm/entities/User';
 
 import AppError from '@shared/errors/AppError';
 import IUsersRepository from '../repositories/IUsersRepository';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 
 interface IRequest {
   user_id: string;
@@ -17,7 +18,10 @@ interface IRequest {
 export default class UpdateUserAvatarService {
   constructor (
     @inject('UsersRepository')
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
   ) {}
   
   public async execute({ user_id, avatarFilename}: IRequest): Promise<User> {
@@ -29,21 +33,14 @@ export default class UpdateUserAvatarService {
     }
 
     if(user.avatar) {
-      // Deletar avatar appointmentsRepository
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-
-      // Verificar se ele existe
-      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
-
-      if(userAvatarFileExists) {
-        await fs.promises.unlink(userAvatarFilePath);
-      }
+      await this.storageProvider.deleteFile(user.avatar);
     }
 
-    user.avatar = avatarFilename;
+    const filename = await this.storageProvider.saveFile(avatarFilename);
+
+    user.avatar = filename;
     await this.usersRepository.save(user);
 
     return user;
-
   }
 }
