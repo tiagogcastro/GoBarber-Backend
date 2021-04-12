b97adb3a1169d44f055745dfd01de0110cc92329
Tiaguin061 <tiaguin180@gmail.com>
2021-04-12 10:15:17 -0300

    feat: Adding generate token

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -3,10 +3,11 @@ import "reflect-metadata";
 import { injectable, inject } from 'tsyringe';
 // import User from '@modules/users/infra/typeorm/entities/User';
 
-// import AppError from '@shared/errors/AppError';
+import AppError from '@shared/errors/AppError';
 
 import IUsersRepository from '../repositories/IUsersRepository';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import IUserTokenRepository from '../repositories/IUserTokensRepository';
 
 interface IRequest {
   email: string;
@@ -19,9 +20,20 @@ export default class SendForgotPasswordEmailService {
 
     @inject('MailProvider')
     private mailProvider: IMailProvider,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokenRepository,
   ) {}
   
   public async execute({email}: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+    
+    if(!user) {
+      throw new AppError('User does not exists');
+    }
+
+    await this.userTokensRepository.generate(user.id);
+
     this.mailProvider.sendMail(email, 'Pedido de recuperação de senha recebido');
   }
 }
