0fb4b1452a0dda633345e54a8318d6eeafebbd79
Tiaguin061 <tiaguin180@gmail.com>
2021-04-12 10:13:51 -0300

    feat: Creating fake user token

---
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -0,0 +1,26 @@
+import User from '../../infra/typeorm/entities/User';
+
+import { uuid } from 'uuidv4';
+import IUserTokenRepository from '../IUserTokensRepository';
+import UserToken from '@modules/users/infra/typeorm/entities/UserToken';
+
+class FakeUserTokensRepository implements IUserTokenRepository {
+  private userTokens: UserToken[] = [];
+
+  public async generate(user_id: string): Promise<UserToken> {
+    const userToken = new UserToken();
+
+    Object.assign(userToken, {
+      id: uuid(),
+      token: uuid(),
+      user_id
+    });
+
+    this.userTokens.push(userToken);
+
+    return userToken;
+  } 
+
+}
+
+export default FakeUserTokensRepository;
