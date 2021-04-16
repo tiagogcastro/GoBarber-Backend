6bfc9415f7abb61945c2a5b4c38b8c1c01ba8463
Tiaguin061 <tiaguin180@gmail.com>
2021-04-16 11:38:10 -0300

    feat: Adding Business rule to token expired

--- src/modules/users/services/ResetPasswordService.ts
+++ src/modules/users/services/ResetPasswordService.ts
@@ -2,11 +2,13 @@ import "reflect-metadata";
 
 import { injectable, inject } from 'tsyringe';
 // import User from '@modules/users/infra/typeorm/entities/User';
+import { isAfter, addHours } from 'date-fns';
 
 import AppError from '@shared/errors/AppError';
 
 import IUsersRepository from '../repositories/IUsersRepository';
 import IUserTokenRepository from '../repositories/IUserTokensRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 
 interface IRequest {
   password: string;
@@ -20,6 +22,9 @@ export default class ResetPasswordService {
 
     @inject('UserTokensRepository')
     private userTokensRepository: IUserTokenRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
   ) {}
   
   public async execute({password, token}: IRequest): Promise<void> {
@@ -36,7 +41,14 @@ export default class ResetPasswordService {
       throw new AppError('User token does not exists');
     }
 
-    user.password = password;
+    const tokenCreatedAt = userToken.created_at;
+    const compareDate = addHours(tokenCreatedAt, 2);
+
+    if(isAfter(Date.now(), compareDate)) {
+      throw new AppError('Token expired');
+    }
+
+    user.password = await this.hashProvider.generateHash(password);
 
     await this.usersRepository.save(user);
 
