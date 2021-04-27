663995e8dbf7d61b034188b45a62cdd084b132a7
Tiaguin061 <tiaguin180@gmail.com>
2021-04-27 11:47:29 -0300

    feat: Created service show profile

---
+++ src/modules/users/services/ShowProfileService.ts
@@ -0,0 +1,29 @@
+import { injectable, inject } from 'tsyringe';
+
+import User from '../infra/typeorm/entities/User';
+
+import AppError from '@shared/errors/AppError';
+import IUsersRepository from '../repositories/IUsersRepository';
+
+interface IRequest {
+  user_id: string;
+}
+
+@injectable()
+export default class ShowProfileService {
+  constructor (
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+  
+  public async execute({ user_id }: IRequest): Promise<User> {
+
+    const user = await this.usersRepository.findById(user_id);
+
+    if(!user) {
+      throw new AppError('Você precisa está logado para atualizar o perfil', 401);
+    }
+
+    return user;
+  }
+}
