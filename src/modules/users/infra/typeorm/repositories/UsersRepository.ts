64268e7ec1cb9aa65fb995aa3909867346b30bc7
Tiaguin061 <tiaguin180@gmail.com>
2021-04-28 10:17:40 -0300

    feat: Added find all providers method for typeorm

--- src/modules/users/infra/typeorm/repositories/UsersRepository.ts
+++ src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -1,10 +1,11 @@
-import { getRepository, Repository } from 'typeorm';
+import { getRepository, Repository, Not } from 'typeorm';
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
 import User from '../entities/User';
 
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+import IFindAllProvidersDTO from '@modules/users/dtos/IFindAllProvidersDTO';
 
 class UsersRepository implements IUsersRepository {
   private ormRepository: Repository<User>
@@ -28,6 +29,21 @@ class UsersRepository implements IUsersRepository {
 
     return user;
   } 
+
+  public async findAllProviders({except_user_id}: IFindAllProvidersDTO): Promise<User[]> {
+    let users: User[];
+    if(except_user_id) {
+      users = await this.ormRepository.find({
+        where: {
+          id: Not(except_user_id),
+        }
+      });
+    } else {
+      users = await this.ormRepository.find();
+    }
+
+    return users;
+  }
 
   public async create(userData: ICreateUserDTO): Promise<User> {
     const user = this.ormRepository.create(userData);
