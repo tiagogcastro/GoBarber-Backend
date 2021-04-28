3b2836a1130405a42da355653607bb2fdb4f6662
Tiaguin061 <tiaguin180@gmail.com>
2021-04-28 14:56:39 -0300

    feat: Added find all in day method on fake repository

--- src/modules/appointments/repositories/fakes/FakeAppontimentRepository.ts
+++ src/modules/appointments/repositories/fakes/FakeAppontimentRepository.ts
@@ -1,10 +1,11 @@
 import { uuid } from 'uuidv4';
-import { isEqual, getMonth, getYear } from 'date-fns'
+import { isEqual, getMonth, getYear, getDate } from 'date-fns'
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
+import IFindAllInDayFromProviderDTO from '@modules/appointments/dtos/IFindAllInDayFromProviderDTO';
 
 class AppointmentsRepository implements IAppointmentsRepository {
   private appointments: Appointment[] = [];
@@ -33,6 +34,25 @@ class AppointmentsRepository implements IAppointmentsRepository {
 
     return appointments
   }
+
+  public async findAllInDayFromProvider({
+    provider_id, month, year, day
+  }: IFindAllInDayFromProviderDTO): Promise<Appointment[]> {
+    const appointments = this.appointments.filter(
+      appointment => {
+        return (
+          appointment.provider_id === 
+          provider_id && 
+          getDate(appointment.date) === day && 
+          getMonth(appointment.date) + 1 === month &&
+          getYear(appointment.date) === year 
+        )
+      }
+    );
+
+    return appointments
+  }
+  
 
   public async create({provider_id, date}: ICreateAppointmentDTO): Promise<Appointment> {
     const appointment = new Appointment();
