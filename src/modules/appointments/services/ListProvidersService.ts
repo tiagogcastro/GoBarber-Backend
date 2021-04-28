88d0fb978135489603e64331e387f919dea009d4
Tiaguin061 <tiaguin180@gmail.com>
2021-04-28 10:16:53 -0300

    feat: Created list providers service

---
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -0,0 +1,31 @@
+import { injectable, inject } from 'tsyringe';
+
+import User from '@modules/users/infra/typeorm/entities/User';
+
+import AppError from '@shared/errors/AppError';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+
+interface IRequest {
+  user_id: string;
+}
+
+@injectable()
+export default class ListProvidersService {
+  constructor (
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+  
+  public async execute({ user_id }: IRequest): Promise<User[]> {
+
+    const user = await this.usersRepository.findAllProviders({
+      except_user_id: user_id,
+    });
+
+    if(!user) {
+      throw new AppError('Você precisa está logado para listar', 401);
+    }
+
+    return user;
+  }
+}
