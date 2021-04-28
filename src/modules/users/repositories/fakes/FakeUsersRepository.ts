f77bdbd3fcf99dddc773b4433181fe1e4bb2dce3
Tiaguin061 <tiaguin180@gmail.com>
2021-04-28 10:19:19 -0300

    feat: Added find all providers on fake repository

--- src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -4,6 +4,7 @@ import User from '../../infra/typeorm/entities/User';
 
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
 import { uuid } from 'uuidv4';
+import IFindAllProvidersDTO from '@modules/users/dtos/IFindAllProvidersDTO';
 
 class UsersRepository implements IUsersRepository {
   private users: User[] = [];
@@ -16,6 +17,16 @@ class UsersRepository implements IUsersRepository {
   public async findByEmail(email: string): Promise<User | undefined> {
     const findUser = this.users.find(user => user.email === email );
     return findUser;
+  }
+
+  public async findAllProviders({except_user_id}: IFindAllProvidersDTO): Promise<User[]> {
+    let users = this.users;
+
+    if(except_user_id) {
+      users = this.users.filter(user => user.id !== except_user_id);
+    }
+
+    return users;
   } 
 
   public async create(userData: ICreateUserDTO): Promise<User> {
