ac7bc2f99be19d95a34028f2d8bc68b932b72c98
Tiaguin061 <tiaguin180@gmail.com>
2021-04-28 14:56:18 -0300

    feat: Added method find all day on interface

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,4 +1,5 @@
 import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
+import IFindAllInDayFromProviderDTO from '../dtos/IFindAllInDayFromProviderDTO';
 import IFindAllInMonthFromProviderDTO from '../dtos/IFindAllInMonthFromProviderDTO';
 import Appointment from '../infra/typeorm/entities/Appointment';
 
@@ -7,4 +8,5 @@ export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
   findAllInMonthFromProvider(data: IFindAllInMonthFromProviderDTO): Promise<Appointment[]>;
+  findAllInDayFromProvider(data: IFindAllInDayFromProviderDTO): Promise<Appointment[]>;
 }
