6372c2bde27e4f128e1363fcb667bc1bde557cf4
Tiaguin061 <tiaguin180@gmail.com>
2021-04-28 10:16:33 -0300

    feat: Created test for list

---
+++ src/modules/appointments/services/ListProvidersService.spec.ts
@@ -0,0 +1,44 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import ListProvidersService from './ListProvidersService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let listProviders: ListProvidersService;
+
+describe('ListProviders', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    listProviders = new ListProvidersService(fakeUsersRepository);
+  });
+  
+  it('Should be able to list the providers', async () => {
+    const user1 = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'teste@example.com',
+      password: '123456'
+    });
+
+    const user2 = await fakeUsersRepository.create({
+      name: 'John Trê',
+      email: 'johntre@example.com',
+      password: '123456'
+    });
+
+    const loggedUser = await fakeUsersRepository.create({
+      name: 'John qua',
+      email: 'johnqua@example.com',
+      password: '123456'
+    });
+
+    const providers = await listProviders.execute({
+      user_id: loggedUser.id,
+    });
+
+    expect(providers).toEqual([
+      user1,
+      user2
+    ]);
+  });
+  
+});
