af9acde6386add6a9a7b95f9e41bc45b5c228216
Tiaguin061 <tiaguin180@gmail.com>
2021-04-28 11:37:23 -0300

    feat: Created provider month availability service

---
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -0,0 +1,39 @@
+import { injectable, inject } from 'tsyringe';
+
+import User from '@modules/users/infra/typeorm/entities/User';
+
+import AppError from '@shared/errors/AppError';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  month: number;
+  year: number;
+}
+
+type IResponse = Array<{
+  day: number;
+  available: boolean;
+}>;
+
+@injectable()
+export default class ListProviderMonthAvailabilityService {
+  constructor (
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository
+  ) {}
+  
+  public async execute({ provider_id, year, month }: IRequest): Promise<IResponse> {
+    const appointments = await this.appointmentsRepository.findAllInMonthFromProvider({
+      provider_id,
+      year,
+      month
+    });
+    console.log(appointments);
+    return [
+      {
+        day: 1, available: false
+      }
+    ]
+  }
+}
