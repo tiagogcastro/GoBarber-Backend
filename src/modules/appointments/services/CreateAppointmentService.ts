506dbfe4f36cb285501f35342f719d408b9ccaac
Tiaguin061 <tiaguin180@gmail.com>
2021-04-30 11:50:04 -0300

    feat: Added latest business rules when creating the appointment

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,4 +1,4 @@
-import { startOfHour} from 'date-fns';
+import { startOfHour, isBefore, getHours } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
@@ -22,10 +22,22 @@ class CreateAppointmentService {
   public async execute({provider_id, date, user_id}: IRequestDTO): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
+    if(isBefore(appointmentDate, Date.now())) {
+      throw new AppError("You can't create an appointments on a past date.");
+    }
+
+    if(user_id === provider_id) {
+      throw new AppError("You can't create an appointment with yourself.")
+    }
+
+    if(getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
+      throw new AppError("You can only create appointments between 8am an 5pm");
+    }
+
     const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(appointmentDate);
 
     if(findAppointmentInSameDate) {
-      throw new AppError("This appointment is already booked.")
+      throw new AppError("This appointment is already booked.");
     }
 
     const appointment = await this.appointmentsRepository.create({
