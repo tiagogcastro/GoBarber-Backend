217e1555a9e348ec27cd82861968a205fa6cfd5e
Tiaguin061 <tiaguin180@gmail.com>
2021-05-04 15:46:16 -0300

    feat: Added router for provider controller

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,18 +1,18 @@
 import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+
 import AppointmentsController from '../controllers/AppointmentsController';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const appointmentsRouter = Router();
-const appointmentsController = new AppointmentsController();
 appointmentsRouter.use(ensureAuthenticated);
 
-// appointmentsRouter.get('/', async (request, response) => {
-//   const appointments = await appointmentsRepository.find();
-
-//   return response.json(appointments);
-// });
+const appointmentsController = new AppointmentsController();
+const providerController = new ProviderAppointmentsController();
 
 appointmentsRouter.post('/', appointmentsController.create);
+
+appointmentsRouter.get('/me', providerController.index);
 
 export default appointmentsRouter;
