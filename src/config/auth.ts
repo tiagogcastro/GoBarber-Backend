ec4082d969b8fedbd461bf83c38b033285ee2938
Tiaguin061 <tiaguin180@gmail.com>
2021-05-08 11:21:23 -0300

    feat: Added ambient variables

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   jwt: {
-    secret: '25a9c640683d8f4f351dcd4f34f9770d',
+    secret: process.env.APP_SECRET,
     expiresIn: '1d',
   }
 }
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,9 @@
 import 'reflect-metadata';
+import 'dotenv/config'
 import express, { Request, Response, NextFunction} from 'express';
 import cors from 'cors';
+import { errors } from 'celebrate/';
+
 import 'express-async-errors';
 
 import routes from '@shared/infra/http/routes';
@@ -17,6 +20,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
+
+app.use(errors());
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
   if(err instanceof AppError) {
