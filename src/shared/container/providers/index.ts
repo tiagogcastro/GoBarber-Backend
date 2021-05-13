641641db3fa03e90df12cb8af76fba7b4da00f32
Tiago <tiaguin180@gmail.com>
2021-05-13 13:43:30 -0300

    feat: Added Ses mail provider on inject dependency

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,9 +1,12 @@
 import { container } from 'tsyringe';
+import mailConfig from '@config/mail';
 
 import IMailProvider from './MailProvider/models/IMailProvider';
 import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
 
 import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
+import SESMailProvider from './MailProvider/implementations/SESMailProvider';
+
 import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
 
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
@@ -21,5 +24,7 @@ container.registerSingleton<IMailTemplateProvider>(
 
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  container.resolve(EtherealMailProvider)
+  mailConfig.driver === 'ethereal' 
+  ? container.resolve(EtherealMailProvider) 
+  : container.resolve(SESMailProvider)
 );
