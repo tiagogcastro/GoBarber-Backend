c0a3f02518c9eabd2da65df6d1e7a80166034ca5
Tiago <tiaguin180@gmail.com>
2021-05-15 11:14:08 -0300

    feat: Using cache provider

--- src/modules/appointments/services/ListProvidersAppointmentsService.ts
+++ src/modules/appointments/services/ListProvidersAppointmentsService.ts
@@ -1,6 +1,7 @@
 import { injectable, inject } from 'tsyringe';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import Appointment from '../infra/typeorm/entities/Appointment';
 
 interface IRequest {
@@ -14,16 +15,23 @@ interface IRequest {
 export default class ListProvidersAppointmentsService {
   constructor (
     @inject('AppointmentsRepository')
-    private appointmentsRepository: IAppointmentsRepository
+    private appointmentsRepository: IAppointmentsRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider
   ) {}
   
   public async execute({ provider_id, year, month, day }: IRequest): Promise<Appointment[]> {
+    const cacheData = await this.cacheProvider.recover('asd');
+    console.log(cacheData);
     const appointments = await this.appointmentsRepository.findAllInDayFromProvider({
       provider_id,
       year,
       month,
       day
     });
+
+    // await this.cacheProvider.save('asd', 'asd');
 
     return appointments;
   }
