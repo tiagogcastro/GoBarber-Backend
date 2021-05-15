1a161908d576b5897041e66a716a6721dccf0fbe
Tiago <tiaguin180@gmail.com>
2021-05-15 11:29:09 -0300

    feat: Using cache provider

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -4,6 +4,7 @@ import User from '@modules/users/infra/typeorm/entities/User';
 
 import AppError from '@shared/errors/AppError';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 interface IRequest {
   user_id: string;
@@ -14,13 +15,22 @@ export default class ListProvidersService {
   constructor (
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider
   ) {}
   
   public async execute({ user_id }: IRequest): Promise<User[]> {
-    const user = await this.usersRepository.findAllProviders({
-      except_user_id: user_id,
-    });
+    let users = await this.cacheProvider.recover<User[]>(`providers-list:${user_id}`);
 
-    return user;
+    if(!users) {
+      users = await this.usersRepository.findAllProviders({
+        except_user_id: user_id,
+      });
+    
+      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+    }
+
+    return users;
   }
 }
