aef3d5bc7824de4a22bc6afebb46380ea1a5411d
Tiaguin061 <tiaguin180@gmail.com>
2021-05-18 12:47:50 -0300

    feat: Creating cache for list providers appointments

--- src/modules/appointments/services/ListProvidersAppointmentsService.ts
+++ src/modules/appointments/services/ListProvidersAppointmentsService.ts
@@ -22,16 +22,19 @@ export default class ListProvidersAppointmentsService {
   ) {}
   
   public async execute({ provider_id, year, month, day }: IRequest): Promise<Appointment[]> {
-    const cacheData = await this.cacheProvider.recover('asd');
-    console.log(cacheData);
-    const appointments = await this.appointmentsRepository.findAllInDayFromProvider({
-      provider_id,
-      year,
-      month,
-      day
-    });
+    const cacheKey = `provider-appointments:${provider_id}:${year}-${month}-${day}`;
+    let appointments = await this.cacheProvider.recover<Appointment[]>(cacheKey);
 
-    // await this.cacheProvider.save('asd', 'asd');
+    if(!appointments) {
+      appointments = await this.appointmentsRepository.findAllInDayFromProvider({
+        provider_id,
+        year,
+        month,
+        day
+      });
+
+      await this.cacheProvider.save(cacheKey, appointments);
+    }
 
     return appointments;
   }
