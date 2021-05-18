f0e92278de51a7f33b1c22d7cad17aac658c9ba4
Tiaguin061 <tiaguin180@gmail.com>
2021-05-18 12:46:57 -0300

    refactor: Removed pasedIso on date

--- src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -10,13 +10,10 @@ export default class AppointmentsController {
     const user_id = request.user.id;
     const {provider_id, date} = request.body;
 
-    // parseIso só esta transformando o dado
-    const parsedDate = parseISO(date);
-  
     const createAppointmentService = container.resolve(CreateAppointmentService);
   
     const appointment = await createAppointmentService.execute({
-      date: parsedDate, 
+      date,
       provider_id,
       user_id
     });
