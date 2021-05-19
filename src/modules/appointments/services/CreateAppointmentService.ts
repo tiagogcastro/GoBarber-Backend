b6889c8d91683768582ac6fce1ec8796a3eda00a
Tiaguin061 <tiaguin180@gmail.com>
2021-05-19 17:20:19 -0300

    feat: Added provider_id on find by date

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -43,7 +43,7 @@ class CreateAppointmentService {
       throw new AppError("You can only create appointments between 8am an 5pm");
     }
 
-    const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(appointmentDate);
+    const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(appointmentDate, provider_id);
 
     if(findAppointmentInSameDate) {
       throw new AppError("This appointment is already booked.");
