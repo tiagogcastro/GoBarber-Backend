a7e551f89d71824c48f8c2d4cee439d179d1be23
Tiaguin061 <tiaguin180@gmail.com>
2021-05-19 17:14:45 -0300

    refactor: Changed request.body to query

--- src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -8,15 +8,15 @@ export default class ProviderAppointmentsController {
   public async index(request: Request, response: Response): Promise<Response> {
     const provider_id = request.user.id;
 
-    const { month, year, day } = request.body;
+    const { month, year, day } = request.query;
 
     const listProviderAppointments = container.resolve(ListProvidersAppointmentsService);
   
     const appointments = await listProviderAppointments.execute({
       provider_id,
-      day,
-      month,
-      year
+      day: Number(day),
+      month: Number(month),
+      year: Number(year)
     });
   
     return response.json(appointments);
--- src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -8,15 +8,15 @@ export default class ProviderDayAvailabilityController {
   public async index(request: Request, response: Response): Promise<Response> {
     const {provider_id} = request.params;
 
-    const { month, year, day } = request.body;
+    const { month, year, day } = request.query;
 
     const listProviderDayAvailability = container.resolve(ListProviderDayAvailabilityService);
   
     const availability = await listProviderDayAvailability.execute({
       provider_id,
-      day,
-      month,
-      year
+      day: Number(day),
+      month: Number(month),
+      year: Number(year)
     });
   
     return response.json(availability);
